bc33bb2beb99fb49b2d484ec0f6493bb3da3bcf6
dvance <dvance@ucar.edu>
2013-11-26 17:43:54 -0700

    initial commit

---
+++ Contents/Code/__init__.py
@@ -0,0 +1,109 @@
+VIDEOS_URL = "http://www.nbcsports.com/ajax-pane/get-pane/3373/61644?/video"
+PLAYER_URL = "http://vplayer.nbcsports.com/p/BxmELC/nbcsportssite/select/"
+THUMB_URL = "http://www.nbcsports.com/files/nbcsports/styles/video_thumbnail/public/media-theplatform/"
+SMIL_URL = "http://link.theplatform.com/s/BxmELC/%s"
+SMIL_NAMESPACE = {'a': 'http://www.w3.org/2005/SMIL21/Language'}
+
+NAME = L('Title')
+ART = 'art-default.jpg'
+ICON = 'icon-default.png'
+
+DEFAULT_LOGO = 'simpsons.jpg'
+LOGOS = {'college-football': True, 'f1': True, 'mlb': True, 'mls': True, 'nba': True, 'nhl': True, 'nfl': True, 'premier-league': True}
+
+####################################################################################################
+def Start():
+
+    Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
+    Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
+
+    # Set the default ObjectContainer attributes
+    ObjectContainer.title1 = NAME
+    ObjectContainer.art = R(ART)
+    ObjectContainer.view_group = "List"
+
+    DirectoryObject.thumb = R(DEFAULT_LOGO)
+    VideoClipObject.thumb = R(ICON)
+
+    # Set the default cache time
+    HTTP.CacheTime = CACHE_1HOUR
+    HTTP.Headers['User-Agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36"
+
+####################################################################################################
+@handler('/video/nbcsports', L('VideoTitle'))
+def MainMenu():
+
+    oc = ObjectContainer()
+
+    # Iterate over all of the available categories and display them to the user.
+    page = HTML.ElementFromURL(VIDEOS_URL)
+    categories = page.xpath('//ul[@class = "video-category-nav"]/li/a')
+
+    for category in categories:
+        cat_id = category.get('href').replace('/video/', '')
+        if cat_id.isdigit():
+            name = category.text
+            logo = name.lower().replace(' ', '-')
+            if logo in LOGOS:
+                logo = R(logo + '.jpg')
+            else:
+                logo = R(DEFAULT_LOGO)
+
+            oc.add(DirectoryObject(key=Callback(ChannelVideoCategory, id=cat_id, name=CleanName(name)), title=name, thumb=logo))
+
+    return oc
+
+####################################################################################################
+@route('/video/nbcsports/{id}')
+def ChannelVideoCategory(id, name):
+
+    oc = ObjectContainer(view_group="InfoList")
+
+    page = HTML.ElementFromURL(VIDEOS_URL)
+
+    thumbs = page.xpath('//div[@id = "channel-' + id + '"]//img//@src')
+    titles = page.xpath('//div[@id = "channel-' + id + '"]//div[@class = "views-field-title"]//a/text()')
+
+    for index in range(len(thumbs)):
+        name = titles[index]
+        thumb = thumbs[index]
+        video_hash = thumb.replace(THUMB_URL, '').split('.', 1)[0]
+
+        # Depending on parameters to SMIL_URL, could be different format, but we'll go with this
+        smil = XML.ElementFromURL(SMIL_URL % video_hash)
+
+        video_details = smil.xpath('//a:video', namespaces=SMIL_NAMESPACE)[0]
+        url = video_details.get('src')
+        summary = video_details.get('abstract')
+        duration = int(video_details.get('duration').strip('ms'))
+        try:
+            tags = [tag.strip() for tag in video_details.get('keywords').split(',')]
+        except:
+            tags = []
+
+        oc.add(VideoClipObject(
+            url=url,
+            title=CleanName(name),
+            thumb=thumb,
+            summary=summary,
+            duration=duration,
+            tags=tags
+        ))
+
+    # It's possible that there is actually no vidoes are available for the ipad. Unfortunately, they
+    # still provide us with empty containers...
+    if len(oc) < 1:
+        return ObjectContainer(header=name, message="There are no titles available for the requested item.")
+
+    return oc
+
+
+####################################################################################################
+def CleanName(name):
+    # Function cleans up HTML ascii stuff
+    remove = [('&amp;', '&'), ('&quot;', '"'), ('&#233;', 'e'), ('&#8212;', ' - '), ('&#39;', '\''), ('&#46;', '.'),
+              ('&#58;', ':'), ('&#8482;', '')]
+    for trash, crap in remove:
+        name = name.replace(trash, crap)
+
+    return name.strip()
